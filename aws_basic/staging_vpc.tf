f29b1c1192e3af65187f0a3ff1eea594212404ce
kiyeongkil <kilkiyoung@gmail.com>
2021-11-23 00:56:26 +0900

    [추가] bations hosts
    - terraform plan 불가
    - bations host 의 subnet_id 문제 < 해결필요

--- aws_basic/staging_vpc.tf
+++ aws_basic/staging_vpc.tf
@@ -1,3 +1,45 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+}
+
+variable "office_cidr_blocks" {
+  type = list
+
+  default = [
+    "0.0.0.0/0" # 이 값은 실제 접속을 허용할 IP를 넣어야 함
+  ]
+}
+
+variable "keypair_name" {
+  default = "2dal-dev"
+}
+
 module "vpc" {
   source = "./vpc"
 
@@ -14,3 +56,19 @@ module "vpc" {
   }
 }
 
+module "bastion" {
+  source = "./bastion"
+
+  name = "bastion"
+  vpc_id = "${module.vpc.vpc_id}"
+
+  ami                 = "${data.aws_ami.amazon_linux.id}"
+  availability_zone   = "ap-northeast-2a"
+  subnet_id           = "${module.vpc.public_subnets_ids[0]}"
+  ingress_cidr_blocks = "${var.office_cidr_blocks}"
+  keypair_name        = "${var.keypair_name}"
+
+  tags = {
+    "TerraformManaged" = "true"
+  }
+}
